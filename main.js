32cc50042f5aeaf30f7d430ec47ce952cc14c3bb
Alan Moreno <alan.moreno@hotmail.com>
2024-03-17 17:39:08 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+//LocalStorage
+
+let database = [];
+
+function addAnAnswer(element){
+    const newAnswer = document.getElementById(element);
+}
+
+function deleteAnswer(){
+    return;
+}
+
+function cleanForm(){
+    return;
+}
+
+function sendForm(){
+    return;
+}
+
+function goToFile(){
+    return;
+}
+
+function downloadFile(){
+    return;
+}
+
+function goToHint(){
+    return;
+}
+
+function goToExplanation(){
+    return;
+}
+
+function goToCardLeft(){
+    return;
+}
+
+function goToCardRight(){
+    return;
+}
+
+//addEventListener()
