1f34e680c5118700f36d2cc3aab29368c0480bdd
Alan Moreno <alan.moreno@hotmail.com>
2024-03-19 23:12:14 -0300

    update icons, styles, javascript

--- main.js
+++ main.js
@@ -1,23 +1,18 @@
 //LocalStorage
+// Salvar uma variável no localStorage
+const minhaVariavel = 'Olá, mundo!';
+localStorage.setItem('minhaChave', minhaVariavel);
 
-let database = [
-    {
-        "id": "0",
-        "origin": "default",
-        "topic": "0",
-        "question": "What is this?",
-        "options": {
-            "1": "a bird",
-            "2": "an instrument",
-            "3": "a game",
-            "4": "a person"
-        },
-        "answers": ["3"],
-        "hints": "it's not boring.",
-        "explanations": "the correct option is \"a game\" because although you can study by here, this is a gamified tool and you always can use to spend free time and make fun with friends."
-    }
-];
+// Obter uma variável do localStorage
+const variavelSalva = localStorage.getItem('minhaChave');
 
+console.log(variavelSalva); // Saída: Olá, mundo!
+
+
+// General variables
+let jsonFile;
+let database = [];
+let filePaths = [];
 
 // navigation
 
@@ -59,7 +54,7 @@ let answerCounter = 4;
 function addAnswer(){
     getAnswers();
     answerCounter++;
-    let answerHTML = `<div class='group-input-answer'><input id='form-checkbox-answer-${answerCounter}' type='checkbox' class='answer-checkbox'><input id='form-field-answer-${answerCounter}' type='text' placeholder='Resposta' class='input-field answer-field'/><button id='form-button-answer-${answerCounter}' class='delete-answer' onclick='deleteAnswer(this);'>×</button></div>`
+    let answerHTML = `<div class='group-input-answer'><input id='form-checkbox-answer-${answerCounter}' type='checkbox' class='answer-checkbox'><input id='form-field-answer-${answerCounter}' type='text' placeholder='Resposta' class='input-field answer-field'/><button id='form-button-answer-${answerCounter}' class='delete-answer button' onclick='deleteAnswer(this);'>×</button></div>`
     addObjectHTML(addAnswerButton, 'beforebegin', answerHTML);
 }
 
@@ -97,7 +92,8 @@ function deleteEmptyAnswers(){
 
 let newId;
 function idGen() {
-    newId = String(parseFloat(database[database.length-1].id) + 1);
+    getAnswers();
+    newId = database.length == 0 ? "1" : String(parseFloat(database[database.length-1].id) + 1);
 }
 
 function sendForm(){
@@ -145,18 +141,64 @@ function readFile(){
     for(i = 0; database.length > i; i++){
         let jsonString = JSON.stringify(database[i], null, 4);
         jsonString = jsonString.replace(/ /g, `&nbsp;`).replace(/\n/g, `<br />`);
-        let divJSON = `<div><div id='page-file-button-${database[i]['id']}' class='page-file-button' onclick='editObject("editObject", ${database[i]['id']});'>${jsonString}</div><button id='page-file-delete-button-${database[i]['id']}' class='delete-object' onclick='deleteObject(${database[i]['id']});'>×</button></div>`;
+        let divJSON = `<div class='page-file-button-container'><div id='page-file-button-${database[i]['id']}' class='page-file-button' onclick='editObject("editObject", ${database[i]['id']});'>${jsonString}</div><button id='page-file-delete-button-${database[i]['id']}' class='delete-object button' onclick='deleteObject(${database[i]['id']});'>×</button></div>`;
         addObjectHTML(pageFile, 'beforeend', divJSON);
     }
 }
 
+function verificateFile() {
+
+}
+
+function updateFile() {
+    jsonFile.database = database;
+}
+
 function saveFile(){
-    return;
+    updateFile();
+    let jsonObject = JSON.stringify(jsonFile, null, 2);
+    let blob = new Blob([jsonObject], { type: 'application/json' });
+    let url = URL.createObjectURL(blob);
+    let a = document.createElement('a');
+    a.href = url;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+}
+
+/*
+function createAndCopyJSONToClipboard(object) {
+    const jsonString = JSON.stringify(object, null, 2);
+    const blob = new Blob([jsonString], { type: 'application/json' });
+    const file = new File([blob], fileName);
+
+    const reader = new FileReader();
+    reader.onload = function(event) {
+        navigator.clipboard.writeText(event.target.result)
+            .then(() => {
+                console.log('Arquivo JSON copiado para a área de transferência. Cole-o no WhatsApp.');
+            })
+            .catch(err => {
+                console.error('Erro ao copiar arquivo JSON para a área de transferência:', err);
+                console.log('Erro ao copiar arquivo JSON para a área de transferência. Por favor, copie manualmente.');
+            });
+    };
+    reader.readAsText(file);
+}
+
+function openWhatsApp() {
+    const whatsappUrl = 'https://web.whatsapp.com/';
+    window.open(whatsappUrl, '_blank');
 }
 
 function shareFile(){
-    return;
+    updateFile();
+    createAndCopyJSONToClipboard(jsonFile);
+    openWhatsApp();
 }
+*/
 
 function deleteObject(idObject){
     database = database.filter((object) => object.id !== String(idObject));
@@ -192,7 +234,84 @@ function editObject(method, idObject){
 }
 
 //screen-player
+
+let orderDatabase;
+
+function orderRandomly(quantity) {
+    let elements = [];
+    for (let i = 1; i <= quantity; i++) {
+        elements.push(i);
+    }
+
+    function shuffle(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
+    return shuffle(elements);
+}
+
+function getCards(){
+    let reorderedElements;
+    if(database){
+        reorderedElements = orderRandomly(10);
+    }else{
+        reorderedElements = orderRandomly(10);
+        console.log("ainda não tem cards");
+    }
+    reorderedElements = orderRandomly(10);
+}
+
+let numberOptions = 1;
+let spanCard = "<span class='single-answer'></span>";
+function numOptions(){
+    numberOptions = 1;
+    switch (numberOptions) {
+        case 1:
+            spanCard = "<span class='single-answer'></span>";
+            break;
+        default:
+            spanCard = "<span class='multi-answer'></span>";
+    }   
+}
+
+function playAndPause(){
+    return;
+}
+
+let numberCards = 0;
+function numCards(){
+    numberCards = document.getElementById('answers-deck').childElementCount;
+}
+
+function pickCard(element) {
+    numOptions();
+    numCards();
+
+    let value = parseInt(element.getAttribute('value'));
+    switch (value) {
+        case 0:
+            if(numberOptions < 2){
+                for(i = 0; numberCards > i; i++){
+                    let deck = document.getElementById('answers-deck').children;
+                    deck[i].setAttribute('value', '0');
+                }
+            }
+            element.setAttribute('value', '1');
+            break;
+        case 1:
+            element.setAttribute('value', '0');
+            break;
+    }
+}
+
 function goToHint(){
+    return;
+}
+
+function confirmQuestion(){
     return;
 }
 
@@ -200,10 +319,69 @@ function goToExplanation(){
     return;
 }
 
-function goToCardLeft(){
+function goToCardFirst(){
+    return;
+}
+function goToCardBefore(){
+    return;
+}
+function moveCardToLast(){
+    return;
+}
+function goToCardNext(){
+    return;
+}
+function goToCardLast(){
     return;
 }
 
-function goToCardRight(){
-    return;
+// screen-overture
+
+let fileName = 'QUIZmeMORE.json';
+let fileInputField = document.getElementById('file-input-field');
+const fileInputButton = document.getElementById('file-input-button');
+const fileNameEditor = document.getElementById('menu-editor-file-name');
+const fileNameReader = document.getElementById('menu-reader-file-name');
+
+function setFileName(nameTheFile){
+    fileName = nameTheFile;
+    fileName = fileName.replace(/\.[^.]*$/, '') + ".json";
+
+    fileInputButton.innerText = fileName;
+    fileNameEditor.innerText = fileName;
+    fileNameReader.innerText = fileName;
+}
+
+const maxPaths = 10;
+function setFilePaths(newPath){
+    if(filePaths.length > maxPaths - 1){
+        filePaths.shift();
+    }
+    if(filePaths.length < maxPaths){
+        filePaths.push(newPath);
+    }
+}
+
+document.getElementById('file-input-field').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(event) {
+            jsonFile = JSON.parse(event.target.result);
+            database = jsonFile.database;
+            setFileName(file.name);
+            setFilePaths(fileInputField.value);
+        };
+        reader.readAsText(file);
+    } else {
+        console.error('Nenhum arquivo selecionado.');
+    }
+});
+
+function goToOverture(){
+    goToScreen('screen-overture');
+}
+function playGame(){
+    goToScreen('screen-player');
 }
